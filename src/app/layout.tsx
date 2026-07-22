aa855d851a437619de1e4f2c3c63e7be649cb6a9
Marcos Makarara <marcosmakarara@gmail.com>
2026-07-22 10:57:53 -0300

    perf: adia inicialização do Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -159,7 +159,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
               strategy="lazyOnload"
             />
-            <Script id="google-analytics" strategy="afterInteractive">
+            <Script id="google-analytics" strategy="lazyOnload">
               {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
